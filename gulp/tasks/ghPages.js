71e618cc7f3d337a2d2980cfe25854e9e1f37d4d
Jon Long <jon@linesandwaves.com>
2014-09-12 17:27:37 -0400

    fixed multi deploy

--- gulp/tasks/ghPages.js
+++ gulp/tasks/ghPages.js
@@ -3,13 +3,13 @@ var shell = require('gulp-shell');
 var config = require('../../config');
 
 gulp.task('ghPages', function() {
-  return gulp.src('**/*', {read: false})
+  return gulp.src(config.paths.src.base, {read: false})
     .pipe(
       shell([
         'git init .',
         'git add .',
         'git commit -m "Deploy"',
-        'git push "git@github.com:jonlong/jonlong.github.io.git" source:master --force',
+        'git push git@github.com:jonlong/jonlong.github.io master:master --force',
         'rm -rf .git'
       ],
       {
