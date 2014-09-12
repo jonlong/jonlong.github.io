df86b972d514d8f887de99ca2d1f2116f2e86211
Jon Long <jon@linesandwaves.com>
2014-09-12 17:21:08 -0400

    add deploy task

--- gulp/tasks/ghPages.js
+++ gulp/tasks/ghPages.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var config = require('../../config');
 
-gulp.task('ghPages', function(callback) {
-  shell.task([
-    'cd ./build',
-    'git init .',
-    'git add .',
-    'git commit -m "Deploy"',
-    'git push "git@github.com:jonlong/writing.git" master:gh-pages --force',
-    'rm -rf .git'
-  ]);
+gulp.task('ghPages', function() {
+  return gulp.src('**/*', {read: false})
+    .pipe(
+      shell([
+        'git init .',
+        'git add .',
+        'git commit -m "Deploy"',
+        'git push "git@github.com:jonlong/jonlong.github.io.git" source:master --force',
+        'rm -rf .git'
+      ],
+      {
+        cwd: config.paths.build.base
+      }
+    )
+  );
 });
