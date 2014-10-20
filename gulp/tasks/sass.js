65480e41021d98405f0b2317f3234d6e49be8d1a
Jon Long <jon@linesandwaves.com>
2014-10-20 02:20:20 -0400

    add pixrem to gulp

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var pixrem = require('gulp-pixrem');
 var config = require('../../config');
 
 gulp.task('sass', function() {
@@ -7,5 +8,6 @@ gulp.task('sass', function() {
     .pipe(sass({
       errLogToConsole: true
     }))
+    .pipe(pixrem())
     .pipe(gulp.dest(config.paths.build.css));
 });
