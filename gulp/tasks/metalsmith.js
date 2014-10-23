ad6748e603232e8b2e686e1d8a32b6133f6ff17e
Jon Long <jon@linesandwaves.com>
2014-10-23 12:56:32 -0400

    set up shortcodes for responsive images

--- gulp/tasks/metalsmith.js
+++ gulp/tasks/metalsmith.js
@@ -1,3 +1,5 @@
+/*jshint multistr: true */
+
 var gulp = require('gulp');
 var filter = require('gulp-filter');
 var gulpsmith = require('gulpsmith');
@@ -8,6 +10,7 @@ var templates = require('metalsmith-templates');
 var collections = require('metalsmith-collections');
 var permalinks = require('metalsmith-permalinks');
 var untemplatize = require('metalsmith-untemplatize');
+var shortcodes = require('metalsmith-flexible-shortcodes');
 var _ = require('lodash');
 var path = require('path');
 var swig = require('swig');
@@ -15,6 +18,26 @@ var config = require('../../config');
 
 var NODE_ENV = process.env.NODE_ENV || 'development';
 var BASE_URL = process.env.BASE_URL || config.blog.baseURL;
+
+var buildResponsiveImageMarkup = function(params) {
+  console.log('up in it')
+  return '<picture>\
+              <!--[if IE 9]><video style="display: none;"><![endif]-->\
+              <source srcset="' + config.paths.build.images + '/' + params.lg + '" media="(min-width: 1000px)">\
+              <source srcset="' + config.paths.build.images + '/' + params.md + '" media="(min-width: 800px)">\
+              <!--[if IE 9]></video><![endif]-->\
+              <img srcset="' + config.paths.build.images + '/' + params.sm + '" alt="' + params.alt + '">\
+          </picture>';
+};
+
+var buildCaptionedImageMarkup = function(params) {
+  var image = buildResponsiveImageMarkup(params);
+
+  return '<figure class="media">\
+              ' + image + '\
+              <figcaption class="media__caption">' + params.caption + '</figcaption>\
+          </figure>';
+};
 
 /**
  * Swig Extensions
@@ -35,7 +58,6 @@ gulp.task('metalsmith', function() {
     .pipe(frontMatter({
       property: 'frontMatter'
     })).on('data', function(file) {
-      console.log(file)
         _.assign(file, file.frontMatter);
         delete file.frontMatter;
     })
@@ -49,6 +71,20 @@ gulp.task('metalsmith', function() {
           now: new Date(),
           blog: config.blog
         })
+        // Use shortcodes for custom markdown tags
+        .use(shortcodes({
+          shortcodes: {
+              'captioned-image': function(str, params) {
+              console.log('responsive image shortcode')
+                var image = buildCaptionedImageMarkup(params);
+                return image;
+            },
+            'image': function(str, params) {
+              var image = buildResponsiveImageMarkup(params);
+              return image;
+            }
+          }
+        }))
         .use(markdown({
           gfm: true,
           smartypants: true
--- src/project.md
+++ src/project.md
@@ -1,5 +1,20 @@
 ---
 template: project.swig
+title: National Geographic
+roles:
+    - Front End Development
+    - Django Admin Design
+url: //nationalgeographic.com
+lead: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consequat nisl dui, quis accumsan mauris efficitur at. Suspendisse lacinia, leo vulputate sodales fringilla, purus odio mollis enim, sit amet mollis magna leo quis sem.
+screens: 
+    - screen1.jpg
+    - screen2.jpg
+    - screen3.jpg
+shortcodes: true
 ---
 
-this is a project
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consequat nisl dui, quis accumsan mauris efficitur at. Suspendisse lacinia, leo vulputate sodales fringilla, purus odio mollis enim, sit amet mollis magna leo quis sem.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consequat nisl dui, quis accumsan mauris efficitur at. Suspendisse lacinia, leo vulputate sodales fringilla, purus odio mollis enim, sit amet mollis magna leo quis sem.
+
+[captioned-image sm="sample-sm.jpg" md="sample-md.jpg" lg="sample-lg.jpg" alt="This is a fake image" caption="This is a caption for the fake image above"]
